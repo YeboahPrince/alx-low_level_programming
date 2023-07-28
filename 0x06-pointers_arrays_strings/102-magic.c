acf0b4bcdc2bec8669d18790e0c72ad5629634b3
YeboahPrince <yeboahprincejr@gmail.com>
2023-07-28 15:54:47 -0700

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int m;
+  int b[5];
+  int *q;
+
+  b[2] = 1024;
+  q = &n;
+  /*
+   * Write your lines of codes here...
+   * Remember:
+   * - you are not allowed to use m
+   * - you are not allowed to modify q
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(q + 5) = 98;
+  /* ...so that this prints 98\m */
+  printf("b[2] = %d\m", a[2]);
+  return (0);
+}
